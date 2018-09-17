1707301ccee553cab1facf9affdaf8331676eb56
Marc Brockschmidt <mabrocks@microsoft.com>
2018-09-17 16:43:20 +0100

    chore(*): Bump version to 0.1.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
       name='dpu_utils',
-      version='0.1.6',
+      version='0.1.7',
       description='Python utilities used by Deep Procedural Intelligence',
       url='https://deepproceduralintelligence.visualstudio.com/dpu-utils/',
       author='Deep Procedural Intelligence',
