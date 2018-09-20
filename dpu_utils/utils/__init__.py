63294d29b0fede18175944166f81a0691ae4a722
Marc Brockschmidt <mabrocks@microsoft.com>
2018-09-20 09:46:34 +0100

    fix(dpu_utils.utils): Export symbols more consistently

--- dpu_utils/utils/__init__.py
+++ dpu_utils/utils/__init__.py
@@ -1,9 +1,13 @@
 from .richpath import RichPath, AzurePath, LocalPath
-from .iterators import BufferedIterator, DoubleBufferedIterator, MultiWorkerCallableIterator
+from .iterators import ThreadedIterator, BufferedIterator, DoubleBufferedIterator, MultiWorkerCallableIterator
 from .dataloading import load_json_gz, save_json_gz, load_jsonl_gz, save_jsonl_gz
 from .gitlog import git_tag_run
 from .debughelper import run_and_debug
 from .chunkwriter import ChunkWriter
 
-__all__ = ['RichPath', 'AzurePath', 'LocalPath', 'BufferedIterator', 'DoubleBufferedIterator', 'MultiWorkerCallableIterator',
-           'git_tag_run', 'run_and_debug', 'ChunkWriter']
+__all__ = ['RichPath', 'AzurePath', 'LocalPath',
+           'ThreadedIterator', 'BufferedIterator', 'DoubleBufferedIterator', 'MultiWorkerCallableIterator',
+           'load_json_gz', 'save_json_gz', 'load_jsonl_gz', 'save_jsonl_gz',
+           'git_tag_run',
+           'run_and_debug',
+           'ChunkWriter']
