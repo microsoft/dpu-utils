4342e725a74a6f6acd122b65f8ff357a3a675747
Miltos Allamanis <miallama@microsoft.com>
2018-10-01 16:45:35 +0100

    Fix cases where cached file is invalid after a failure to retrieve it.
    In some cases, such as authentication failures, the BlobService
    creates an empty file, before failing. Catch the exception, delete
    the file and re-raise it.

--- dpu_utils/utils/richpath.py
+++ dpu_utils/utils/richpath.py
@@ -248,8 +248,13 @@ class AzurePath(RichPath):
     def __cache_file_locally(self) -> LocalPath:
         cached_file_path = os.path.join(self.__cache_location, self.__container_name, self.path)
         if not os.path.isfile(cached_file_path):
-            os.makedirs(os.path.dirname(cached_file_path), exist_ok=True)
-            self.__blob_service.get_blob_to_path(self.__container_name, self.path, cached_file_path)
+            try:
+                os.makedirs(os.path.dirname(cached_file_path), exist_ok=True)
+                self.__blob_service.get_blob_to_path(self.__container_name, self.path, cached_file_path)
+            except Exception as e:
+                if os.path.exists(cached_file_path):
+                    os.remove(cached_file_path)   # On failure, remove the cached file, if it exits.
+                raise e
         return LocalPath(cached_file_path)
 
     def __read_as_binary(self) -> bytes:
