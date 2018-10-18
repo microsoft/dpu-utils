d79c186596fc52698fbae48eff4149785d837cc5
Miltos Allamanis <miallama@microsoft.com>
2018-10-18 15:33:58 +0100

    Fix package info

--- setup.py
+++ setup.py
@@ -5,9 +5,8 @@ setuptools.setup(
       version='0.1.15',
       license='MIT',
       description='Python utilities used by Deep Procedural Intelligence',
-      url='https://deepproceduralintelligence.visualstudio.com/dpu-utils/',
+      url='https://github.com/microsoft/dpu-utils',
       author='Deep Procedural Intelligence',
-      author_email='miallama@microsoft.com',
       packages=setuptools.find_packages(),
       install_requires=[
           'azure-storage', 'numpy'
