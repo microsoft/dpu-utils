7f793257c830bec9bec1675d4894990c5ecdccfb
Miltos Allamanis <miallama@microsoft.com>
2018-10-25 11:08:30 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
       name='dpu_utils',
-      version='0.1.15',
+      version='0.1.16',
       license='MIT',
       description='Python utilities used by Deep Procedural Intelligence',
       url='https://github.com/microsoft/dpu-utils',
