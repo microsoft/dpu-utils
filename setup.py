aef2386ac75f13eeace9916c387cc874da79df9c
Miltos Allamanis <miallama@microsoft.com>
2018-11-01 15:23:33 +0000

    Improve setup.py information

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 import setuptools
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setuptools.setup(
       name='dpu_utils',
-      version='0.1.18',
+      version='0.1.19',
       license='MIT',
       description='Python utilities used by Deep Procedural Intelligence',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/microsoft/dpu-utils',
       author='Deep Procedural Intelligence',
       packages=setuptools.find_packages(),
