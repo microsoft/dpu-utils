d45581474415902ac9ca744846c511d1e525b497
Miltos Allamanis <miallama@microsoft.com>
2018-11-19 19:07:30 +0000

    Fixes

--- dpu_utils/codeutils/keywords/keywordlist.py
+++ dpu_utils/codeutils/keywords/keywordlist.py
@@ -5,7 +5,7 @@ from typing import List
 
 def load_file(name: str) -> List[str]:
     with open(os.path.join(os.path.dirname(__file__), name)) as f:
-        return [l.strip() for l in f]
+        return [l.strip() for l in f if len(l.strip()) > 0]
 
 
 def get_language_keywords(language: str) -> List[str]:
