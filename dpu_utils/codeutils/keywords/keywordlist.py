b8abf4d6d52b556f474a55eb526a12d943b7b9a3
Miltos Allamanis <miallama@microsoft.com>
2019-05-05 22:14:17 -0500

    Add keyword lists for more languages.

--- dpu_utils/codeutils/keywords/keywordlist.py
+++ dpu_utils/codeutils/keywords/keywordlist.py
@@ -13,9 +13,20 @@ def load_file(name: str) -> FrozenSet[str]:
 
 @lru_cache()
 def get_language_keywords(language: str) -> FrozenSet[str]:
+    """
+    Returns the keywords of a programming language.
+
+    There are some inconsistencies across languages wrt to
+    what is considered a keyword. For example, the true/false
+    literals are considered keywords in many languages. However,
+    we exclude them here for consistency. We also exclude special
+    functions-like keywords, such as `die()` in PHP.
+    """
     if language == 'c':
         return load_file('c.txt')
-    elif language == 'csharp':
+    elif language == 'cpp' or language == 'c++':
+        return load_file('cpp.txt')
+    elif language == 'csharp' or language == 'c#':
         return load_file('csharp.txt')
     elif language == 'go':
         return load_file('go.txt')
@@ -23,7 +34,13 @@ def get_language_keywords(language: str) -> FrozenSet[str]:
         return load_file('java.txt')
     elif language == 'javascript':
         return load_file('javascript.txt')
+    elif language == 'php':
+        return load_file('php.txt')
     elif language == 'python':
         return frozenset(k for k in keyword.kwlist if k != 'True' and k != 'False')
+    elif language == 'ruby':
+        return load_file('ruby.txt')
+    elif language == 'typescript':
+        return load_file('typescript.txt')
     else:
         raise Exception('Language %s not supported yet' % language)
