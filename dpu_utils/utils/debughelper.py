f109e2e1d0166d1f9b41ced0233e6adab26b4d53
Miltos <miallama@microsoft.com>
2019-05-15 17:15:15 +0100

    Add documentation

--- dpu_utils/utils/debughelper.py
+++ dpu_utils/utils/debughelper.py
@@ -6,7 +6,17 @@ from typing import Callable
 __all__= ['run_and_debug']
 
 
-def run_and_debug(func: Callable[[], None], enable_debugging: bool)-> None:
+def run_and_debug(func: Callable[[], None], enable_debugging: bool) -> None:
+    """
+    A wrapper around a running script that triggers the debugger in case of an uncaught exception.
+    
+    For example, this can be used as:
+    ```
+    if __name__ == '__main__':
+        args = docopt(__doc__)
+        run_and_debug(lambda: run(args), args['--debug'])
+    ```
+    """
     try:
         func()
     except:
