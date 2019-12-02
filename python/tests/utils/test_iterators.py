cce2140a7b89794c59db2d4b935b4e2649c1ddae
Miltos Allamanis <miallama@microsoft.com>
2019-12-02 12:25:22 +0000

    Update test

--- python/tests/utils/test_iterators.py
+++ python/tests/utils/test_iterators.py
@@ -5,8 +5,10 @@ from dpu_utils.utils.iterators import shuffled_iterator
 
 class TestShuffleIterator(unittest.TestCase):
 
-    def test_return_all(self):
-        for size in [10, 10000, 100000]:
-            shuffled_dataset = set(shuffled_iterator(range(size)))
-            self.assertSetEqual(shuffled_dataset, set(range(size)), f'Some returned elements are missing.')
+    def test_return_all_elements(self):
+        for size in [100, 10000, 100000]:
+            shuffled_dataset = list(shuffled_iterator(range(size)))
+            self.assertNotEqual(shuffled_dataset[:10], list(range(10)),
+                                'It is highly unlikely that the original order is preserved')
+            self.assertSetEqual(set(shuffled_dataset), set(range(size)), f'Some returned elements are missing.')
 
