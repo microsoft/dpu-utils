c2c20321a5d2f49f08dd2aad969e2d5a00ac0b38
Miltos <miallama@microsoft.com>
2019-12-11 09:06:26 +0000

    Bump Version

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ with open('../README.md') as f:
 
 setuptools.setup(
       name='dpu_utils',
-      version='0.1.38',
+      version='0.1.39',
       license='MIT',
       description='Python utilities used by Deep Procedural Intelligence',
       long_description=long_description,
