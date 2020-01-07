c36197bfbe499bea42e5e63d496e75658c2dd045
Miltos Allamanis <miallama@microsoft.com>
2020-01-07 13:47:38 +0000

    Rudimentary test for other iterators.

--- python/tests/utils/test_iterators.py
+++ python/tests/utils/test_iterators.py
@@ -1,6 +1,6 @@
 import unittest
 
-from dpu_utils.utils import shuffled_iterator
+from dpu_utils.utils import shuffled_iterator, ThreadedIterator, BufferedIterator, DoubleBufferedIterator
 
 
 class TestShuffleIterator(unittest.TestCase):
@@ -12,3 +12,13 @@ class TestShuffleIterator(unittest.TestCase):
                                 'It is highly unlikely that the original order is preserved')
             self.assertSetEqual(set(shuffled_dataset), set(range(size)), f'Some returned elements are missing.')
 
+
+class TestParellelIterators(unittest.TestCase):
+
+    ALL_ITERATOR_TYPES = [ThreadedIterator, BufferedIterator, DoubleBufferedIterator]
+
+    def test_return_all_elements_in_order(self):
+        for iterator_type in self.ALL_ITERATOR_TYPES:
+            for size in [100, 10000]:
+                returned = list(iterator_type(range(size)))
+                self.assertListEqual(returned, list(range(size)), f'Iterator {iterator_type.__name__} did not return all elements.')
