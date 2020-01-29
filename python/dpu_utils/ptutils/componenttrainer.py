74c0f1c28646438596ad4118878be24b0412ef69
Miltos Allamanis <miallama@microsoft.com>
2020-01-29 14:04:44 +0000

    Update hook interfaces based on comments.

--- python/dpu_utils/ptutils/componenttrainer.py
+++ python/dpu_utils/ptutils/componenttrainer.py
@@ -1,7 +1,8 @@
 import json
 import logging
 import time
-from typing import Optional, Iterable, Set, TypeVar, Generic, Callable, List, Dict, Iterator, Tuple, Union
+from abc import ABC, abstractmethod
+from typing import Optional, Iterable, Set, TypeVar, Generic, Callable, List, Dict, Iterator, Tuple, Union, Any
 
 import torch
 from tqdm import tqdm
@@ -14,7 +15,12 @@ InputData = TypeVar('InputData')
 TensorizedData = TypeVar('TensorizedData')
 EndOfEpochHook = Callable[[BaseComponent, int, Dict], None]
 
-__all__ = ['ComponentTrainer']
+__all__ = ['ComponentTrainer', 'AbstractScheduler']
+
+class AbstractScheduler(ABC):
+    @abstractmethod
+    def step(self, epoch_idx: int, epoch_step: int)-> None:
+        pass
 
 class ComponentTrainer(Generic[InputData, TensorizedData]):
     """
@@ -30,7 +36,20 @@ class ComponentTrainer(Generic[InputData, TensorizedData]):
     def __init__(self, model: BaseComponent[InputData, TensorizedData], save_location: RichPath,
                  *, max_num_epochs: int = 200, minibatch_size: int = 200,
                  optimizer_creator: Optional[Callable[[Iterable[torch.Tensor]], torch.optim.Optimizer]]=None,
-                 scheduler_creator: Optional[Callable[[torch.optim.Optimizer], torch.optim.lr_scheduler._LRScheduler]]=None):
+                 scheduler_creator: Optional[Callable[[torch.optim.Optimizer], AbstractScheduler]]=None):
+        """
+
+        :param model: The model to be trained.
+        :param save_location: The location where the trained model will be checkpointed and saved.
+        :param max_num_epochs: The maximum number of epochs to run training for.
+        :param minibatch_size: The maximum size of the minibatch (`BaseComponent`s can override this
+            by detecting full minibatches and returning False in `extend_minibatch_by_sample`)
+        :param optimizer_creator: An optional function that accepts an iterable of the training parameters
+            (pyTorch tensors) and returns a PyTorch optimizer.
+        :param scheduler_creator: An optional function that accepts an optimizer and creates a scheduler
+            implementing `AbstractScheduler`. This could be a wrapper for existing learning schedulers.
+            The scheduler will be invoked at after each training step.
+        """
         self.__model = model
         self.__save_location = save_location
         assert save_location.path.endswith('.pkl.gz'), 'All models are stored as .pkl.gz. Please indicate this in the save_location.'
@@ -47,7 +66,7 @@ class ComponentTrainer(Generic[InputData, TensorizedData]):
         self.__train_epoch_end_hooks = []  # type: List[EndOfEpochHook]
         self.__validation_epoch_end_hooks = [] # type: List[EndOfEpochHook]
         self.__metadata_finalized_hooks = []  # type: List[Callable[[BaseComponent], None]]
-        self.__training_start_hooks = []   # type: List[Callable[[BaseComponent], None]]
+        self.__training_start_hooks = []   # type: List[Callable[[BaseComponent, torch.optim.Optimizer], None]]
 
     @property
     def model(self) -> BaseComponent[InputData, TensorizedData]:
@@ -81,7 +100,7 @@ class ComponentTrainer(Generic[InputData, TensorizedData]):
     def register_model_metadata_finalized_hook(self, hook: Callable[[BaseComponent], None]) -> None:
         self.__metadata_finalized_hooks.append(hook)
 
-    def register_training_start_hook(self, hook: Callable[[BaseComponent], None]) -> None:
+    def register_training_start_hook(self, hook: Callable[[BaseComponent, torch.optim.Optimizer], None]) -> None:
         self.__training_start_hooks.append(hook)
 
     def train(self, training_data: Iterable[InputData], validation_data: Iterable[InputData],
@@ -143,7 +162,7 @@ class ComponentTrainer(Generic[InputData, TensorizedData]):
         scheduler = None if self.__create_scheduler is None else self.__create_scheduler(optimizer)
 
         for hook in self.__training_start_hooks:
-            hook(self.__model)
+            hook(self.__model, optimizer)
 
         best_loss = float('inf')  # type: float
         num_epochs_not_improved = 0  # type: int
@@ -159,16 +178,16 @@ class ComponentTrainer(Generic[InputData, TensorizedData]):
             start_time = time.time()
             self.__model.reset_metrics()
             with tqdm(desc='Training', disable=not show_progress_bar, leave=False) as progress_bar:
-                for mb_data, num_elements in ThreadedIterator(
+                for step_idx, (mb_data, num_elements) in enumerate(ThreadedIterator(
                         minibatch_iterator(data_iter, return_partial_minibatches=False),
-                        enabled=parallel_minibatch_creation):
+                        enabled=parallel_minibatch_creation)):
                     optimizer.zero_grad()
                     mb_loss = self.__model(**mb_data)
                     mb_loss.backward()
 
                     optimizer.step()
                     if scheduler is not None:
-                        scheduler.step()
+                        scheduler.step(epoch_idx=epoch, epoch_step=step_idx)
 
                     num_minibatches += 1
                     num_samples += num_elements
