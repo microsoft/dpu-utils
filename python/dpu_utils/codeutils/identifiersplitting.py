c188de06c7ce19cce19c6cac868c1d31a852a062
Miltos Allamanis <miallama@microsoft.com>
2021-03-04 11:24:46 +0000

    Handle discrepancy between Py3.6 and Py3.7 wrt to regex splitting.

--- python/dpu_utils/codeutils/identifiersplitting.py
+++ python/dpu_utils/codeutils/identifiersplitting.py
@@ -1,15 +1,21 @@
-import re
 from functools import lru_cache
 from typing import List
+import sys
 
+REGEX_TEXT = ("(?<=[a-z0-9])(?=[A-Z])|"
+              "(?<=[A-Z0-9])(?=[A-Z][a-z])|"
+              "(?<=[0-9])(?=[a-zA-Z])|"
+              "(?<=[A-Za-z])(?=[0-9])|"
+              "(?<=[@$])(?=[a-zA-Z0-9])|"
+              "(?<=[a-zA-Z0-9])(?=[@$])|"
+              "_")
 
-SPLIT_REGEX = re.compile("(?<=[a-z0-9])(?=[A-Z])|"
-                         "(?<=[A-Z0-9])(?=[A-Z][a-z])|"
-                         "(?<=[0-9])(?=[a-zA-Z])|"
-                         "(?<=[A-Za-z])(?=[0-9])|"
-                         "(?<=[@$])(?=[a-zA-Z0-9])|"
-                         "(?<=[a-zA-Z0-9])(?=[@$])|"
-                         "_")
+if sys.version_info >= (3, 7):
+    import re
+    SPLIT_REGEX = re.compile(REGEX_TEXT)
+else:
+    import regex
+    SPLIT_REGEX = regex.compile("(?V1)"+REGEX_TEXT)
 
 
 @lru_cache(maxsize=5000)
