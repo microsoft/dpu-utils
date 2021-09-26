72cac67490ccf19fede6b86269c57843549b526c
Miltos <miallama@microsoft.com>
2021-09-26 09:38:27 +0100

    Bump version

--- python/setup.py
+++ python/setup.py
@@ -8,7 +8,7 @@ else:
 
 setuptools.setup(
       name='dpu_utils',
-      version='0.6.0',
+      version='0.6.1',
       license='MIT',
       description='Python utilities used by Deep Procedural Intelligence',
       long_description=long_description,
